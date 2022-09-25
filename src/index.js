b190ceebb301ab5a72a12715badab9d673182a46
Jwade1781 <jwade5219@gmail.com>
2022-09-24 23:40:02 -0400

    Redesign of UI

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './css/style.css';
-import domController from './domController'
 
 function main(){
     console.log("Main Start");
-    domController().start();
+    //domController().start();
 }
 
 window.addEventListener("load", () => main());
